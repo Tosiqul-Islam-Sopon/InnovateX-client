c1e3f2da8fa778c9424fda60d4928c320b32b2b7
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-06-03 16:44:16 +0600

    all setup done

---
+++ src/Pages/User/Home/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-red-500">Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./Pages/ShareAll/ErrorPage";
+import Root from "./Pages/Root/Root";
+import Home from "./Pages/User/Home/HomePage/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage></ErrorPage>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
